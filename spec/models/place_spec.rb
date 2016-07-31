cc6f07950a39d054883906c5328c4349b95df482
Brent Martin <bmart35@gmail.com>
2016-07-30 22:59:27 -0500

    Test the creation of the object Place - passed

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Place, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "creation" do
+    it "should have one item created after being created" do
+      @place = Place.create(name: "test name", address: "100 Example St, City, Tx 78701", description: "test description", hours: "X until X")
+      expect(Place.all.count).to eq(1)
+    end
+  end
 end
