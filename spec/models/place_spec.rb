5133a98135ad150a91070cadb22d8980015a56bb
Brent Martin <bmart35@gmail.com>
2016-07-30 23:53:26 -0500

    refactoring - remove duplication of creating a Place each time by including a before block

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -2,30 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Place, type: :model do
 
+  before(:each) do
+    @place = Place.create!(name: "test name", address: "100 Example St, City, Tx 78701", description: "test description", hours: "X until X")
+  end
+
   describe "creation" do
     it "should have one item created after being created" do
-      @place = Place.create(name: "test name", address: "100 Example St, City, Tx 78701", description: "test description", hours: "X until X")
       expect(Place.all.count).to eq(1)
     end
 
     context "when validating new Place" do
       it "should not validate without a name" do
-        @place = Place.create(name: nil, address: "100 Example St, City, Tx 78701", description: "test description", hours: "X until X")
         expect(@place).to_not be_valid
       end
 
       it "should not validate without an address" do
-        @place = Place.create(name: "test name", address: nil, description: "test description", hours: "X until X")
         expect(@place).to_not be_valid
       end
 
       it "should allow validation without description" do
-        @place = Place.create(name: "test name", address: "100 Example St, City, Tx 78701", description: nil, hours: "X until X")
         expect(@place).to be_valid
       end
 
       it "should allow validation without hours" do
-        @place = Place.create(name: "test name", address: "100 Example St, City, Tx 78701", description: "test description", hours: nil)
         expect(@place).to be_valid
       end
     end
