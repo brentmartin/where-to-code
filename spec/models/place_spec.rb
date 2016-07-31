4c9482befb0ae083a30b667c488a45a93ee5fe9b
Brent Martin <bmart35@gmail.com>
2016-07-30 23:05:57 -0500

    included a context around validating a new Place object

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -7,5 +7,8 @@ RSpec.describe Place, type: :model do
       @place = Place.create(name: "test name", address: "100 Example St, City, Tx 78701", description: "test description", hours: "X until X")
       expect(Place.all.count).to eq(1)
     end
+
+    context "when validating new Place" do
+  end
   end
 end
