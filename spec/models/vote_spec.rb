7f7089e98a1fa0623f07bfc1b01e56075d85d080
Brent Martin <bmart35@gmail.com>
2016-08-01 02:13:22 -0500

    refactoring - set nils, and necessary testing content for each test

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -13,25 +13,30 @@ RSpec.describe Vote, type: :model do
 
     context "when creating a new Vote" do
       it "should not let a Vote be created without a pick" do
+        @vote.pick = nil
         expect(@vote).to_not be_valid
       end
 
       context "that is for a Place" do
         it "should not let a Vote be created without a place_id" do
+          @vote.place_id = nil
           expect(@vote).to_not be_valid
         end
 
         it "should be a Place already created that has an ID that matches the place_id of the Vote"
       context "that needs a valid option for pick" do
         it "should not accept an invalid entry" do
+          @vote.pick = "invalid_vote"
           expect(@vote).to_not be_valid
         end
 
         it "should accept the entry 'up_vote'" do
+          @vote.pick = "up_vote"
           expect(@vote).to be_valid
         end
 
         it "should accept the entry 'down_vote'" do
+          @vote.pick = "down_vote"
           expect(@vote).to be_valid
         end
       end
