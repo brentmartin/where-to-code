45b5973cfbe3d977b9fe2f9b22f9a97f1f4e795f
Brent Martin <bmart35@gmail.com>
2016-07-31 22:48:05 -0500

    refactoring - remove duplication of creating an Opinion each time by including a before block

--- spec/models/opinion_spec.rb
+++ spec/models/opinion_spec.rb
@@ -2,36 +2,34 @@ require 'rails_helper'
 
 RSpec.describe Opinion, type: :model do
 
+  before(:each) do
+    @opinion = Opinion.create!(head: "opinion test head", body: "opinion test body, testing the body of the content", place_id: 1)
+  end
+
   describe "creation" do
     it "should have one item created after being created" do
-      @opinion = Opinion.create(head: "opinion test head", body: "opinion test body, testing the body of the content", place_id: 1)
       expect(Opinion.all.count).to eq(1)
     end
 
     context "when creating a new Opinion" do
       it "should not let an Opinion be created without a body" do
-        @opinion = Opinion.create(head: "opinion test head", body: nil)
         expect(@opinion).to_not be_valid
       end
 
       it "should not let an Opinion be created without an ID associated to a Place" do
         @place = Place.create(name: "test name", address: "100 Example St, City, Tx 78701", description: "test description", hours: "X until X")
-        @opinion = Opinion.create(head: "opinion test head", body: "opinion test body, testing the body of the content", place_id: nil)
         expect(@opinion).to_not be_valid
       end
 
       it "should allow a Opinion to be created without a head" do
-        @opinion = Opinion.create(head: nil, body: "opinion test body, testing the body of the content", place_id: 1)
         expect(@opinion).to be_valid
       end
 
       it "should not accept body of an Opinion with less than 1 character" do
-        @opinion = Opinion.create(head: "opinion test head", body: "", place_id: 1)
         expect(@opinion).to_not be_valid
       end
 
       it "should not accept body of an Opinion with more than 160 characters" do
-        @opinion = Opinion.create(head: "opinion test head", body: "the example for this string is exactly 161 characters, which is enough to make this test pass. One less character, in other words 160 characters, would break it.", place_id: 1)
         expect(@opinion).to_not be_valid
       end
     end
