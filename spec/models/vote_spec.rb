03b9576697f64d11a76139aa30b8897bf3dc4ca5
Brent Martin <bmart35@gmail.com>
2016-08-01 00:30:13 -0500

    test creation of an Vote - passed

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
+
+  describe "creation" do
+    it "should have one item created after being created" do
+      @vote = Vote.create!(pick: "example_vote")
+      expect(Vote.all.count).to eq(1)
+    end
 end
