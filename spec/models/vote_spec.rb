16ad9cfad24b008c65bb69b9437b18778172fa14
Brent Martin <bmart35@gmail.com>
2016-08-01 00:31:22 -0500

    test to see if Place is correctly associated with Vote model - failed

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe Vote, type: :model do
       @vote = Vote.create!(pick: "example_vote")
       expect(Vote.all.count).to eq(1)
     end
+  describe "associations" do
+    context "when setting up db schema for Vote" do
+      it "should belong to places" do
+        @vote = Vote.reflect_on_association(:places)
+        @vote.macro.should == :belongs_to
+      end
+    end
+  end
 end
