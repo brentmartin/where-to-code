2415916e65b236a8183c34352bf6e96a28b6411d
Brent Martin <bmart35@gmail.com>
2016-07-31 21:15:10 -0500

    test creation of an Opinion - passed

--- spec/models/opinion_spec.rb
+++ spec/models/opinion_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Opinion, type: :model do
+
+  describe "creation" do
+    it "should have one item created after being created" do
+      @opinion = Opinion.create(head: "opinion test head", body: "opinion test body, testing the body of the content")
+      expect(Opinion.all.count).to eq(1)
+    end
+  end
 end
