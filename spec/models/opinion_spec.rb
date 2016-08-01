f1401402edcef0fbd4168b0525f6c35d6d70b5aa
Brent Martin <bmart35@gmail.com>
2016-07-31 21:34:07 -0500

    test to see if Place is correctly associated with Opinion model - failed

--- spec/models/opinion_spec.rb
+++ spec/models/opinion_spec.rb
@@ -7,5 +7,15 @@ RSpec.describe Opinion, type: :model do
       @opinion = Opinion.create(head: "opinion test head", body: "opinion test body, testing the body of the content")
       expect(Opinion.all.count).to eq(1)
     end
+
+    describe "associations" do
+      context "when setting up db schema for Opinion" do
+        it "should belong to places" do
+          @opinion = Opinion.reflect_on_association(:places)
+          @opinion.macro.should == :belongs_to
+        end
+
+      end
+    end
   end
 end
