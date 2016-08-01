7a45ad0575f9e4d089e6d2005534c72ac73dea30
Brent Martin <bmart35@gmail.com>
2016-07-31 23:09:53 -0500

    refactoring - combined Place validation code for name and address into one line

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,5 @@ class Place < ActiveRecord::Base
   has_many :opinions
   has_many :votes
 
-  validates_presence_of :name
-  validates_presence_of :address
+  validates_presence_of :name, :address
 end
