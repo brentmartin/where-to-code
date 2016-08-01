be6d098ec3349f10b3c3e055ae753cada65b8e2e
Brent Martin <bmart35@gmail.com>
2016-08-01 00:36:50 -0500

    test necessary validation of place_id - failed

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -13,6 +13,12 @@ RSpec.describe Vote, type: :model do
         @vote = Vote.create(pick: nil)
         expect(@vote).to_not be_valid
       end
+
+      context "that is for a Place" do
+        it "should not let a Vote be created without a place_id" do
+          @vote = Vote.create(pick: "example_vote", place_id: nil)
+          expect(@vote).to_not be_valid
+        end
   end
   describe "associations" do
     context "when setting up db schema for Vote" do
