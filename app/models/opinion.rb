932ccc14090c69b3e8be38065d216f56a6ae23af
Brent Martin <bmart35@gmail.com>
2016-07-31 23:09:28 -0500

    refactoring - combined Opinion validation code for body and place_id into one line

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,7 +1,6 @@
 class Opinion < ActiveRecord::Base
   belongs_to :places
 
-  validates_presence_of :body
-  validates_presence_of :place_id
+  validates_presence_of :body, :place_id
   validates_length_of :body, minimum: 1, maximum: 160, allow_blank: true
 end
