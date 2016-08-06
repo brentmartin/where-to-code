48e20a3f2580ba7d9b6ef1d66a4b9d7110f44b86
Brent Martin <bmart35@gmail.com>
2016-08-06 12:43:59 -0500

    refactoring - remove duplication of User creation by including before block

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,20 +2,21 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  before(:each) do
+    @user = User.create!(name: "Brent", email: "test@example.com", password: "password")
+  end
+
   describe "creation" do
     it "should have one item created after being created" do
-    @user = User.create!(email: "test@example.com", password: "password", name: "Brent")
       expect(User.all.count).to eq(1)
     end
 
     context "when creating a new User" do
       it "should not let a User be created without a name" do
-        @user = User.create(email: "test@example.com", password: "password", name: nil)
         expect(@user).to_not be_valid
       end
 
       it "should not let a User be created without an email address" do
-        @user = User.new(email: nil, password: "password", name: "Brent")
         expect(@user).to_not be_valid
       end
 
