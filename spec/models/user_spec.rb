3d46289ec431aa0de87b181b953d0772e3f69364
Brent Martin <bmart35@gmail.com>
2016-08-06 11:23:29 -0500

    test creation of a User - passed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  describe "creation" do
+    it "should have one item created after being created" do
+    @user = User.create!(email: "test@example.com", password: "password")
+      expect(User.all.count).to eq(1)
+    end
 end
