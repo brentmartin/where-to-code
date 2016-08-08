84b3ad2db7b181dce46b80ee44eaef7ca6168ee6
Brent Martin <bmart35@gmail.com>
2016-08-07 23:55:34 -0500

    wrote code to associate an Opinion to belong to a User

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,5 +1,6 @@
 class Opinion < ActiveRecord::Base
   belongs_to :places
+  belongs_to :users
 
   validates_presence_of :body, :place_id
   validates_length_of :body, minimum: 1, maximum: 160, allow_blank: true
