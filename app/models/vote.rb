f2178203d9339c0687dbe29710b58982fb5b5266
Brent Martin <bmart35@gmail.com>
2016-08-08 00:21:53 -0500

    wrote code to make test on user_id pass

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,6 +2,7 @@ class Vote < ActiveRecord::Base
   belongs_to :places
   belongs_to :users
 
+  validates_presence_of :user_id
   validates_presence_of :pick, :place_id
   validates_inclusion_of :pick, in: ["up_vote", "down_vote"]
 end
