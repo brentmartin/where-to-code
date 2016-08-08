54a38aa2d0b0e941f1a7e65b8a84906fa853653e
Brent Martin <bmart35@gmail.com>
2016-08-08 00:24:46 -0500

    refactoring - combined user id's into the validates_presence_of blocks for Opinions and Vote models

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -2,7 +2,6 @@ class Opinion < ActiveRecord::Base
   belongs_to :places
   belongs_to :users
 
-  validates_presence_of :user_id
-  validates_presence_of :body, :place_id
+  validates_presence_of :body, :place_id, :user_id
   validates_length_of :body, minimum: 1, maximum: 160, allow_blank: true
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,7 +2,6 @@ class Vote < ActiveRecord::Base
   belongs_to :places
   belongs_to :users
 
-  validates_presence_of :user_id
-  validates_presence_of :pick, :place_id
+  validates_presence_of :pick, :place_id, :user_id
   validates_inclusion_of :pick, in: ["up_vote", "down_vote"]
 end
