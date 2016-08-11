e12311e0b9844716b3946b285fc8e91a88fb65c6
Brent Martin <bmart35@gmail.com>
2016-08-10 23:43:00 -0500

    wrote failing test for returning a place

--- spec/controllers/places_controller_spec.rb
+++ spec/controllers/places_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe PlacesController, type: :controller do
+  render_views
+
+  describe "GET /places" do
+    it "returns a place" do
+      Place.create(name: "test name", address: "100 Example St, City, Tx 78701")
+      get :index
+      expect(response.body).to include("test name")
+    end
+
+  end
 
 end
